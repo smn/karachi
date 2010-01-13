ff799cee9f8261304af6b42882c6f259c8fa5c40
Simon de Haan <simon@soocial.com>
2010-01-13 23:18:34 +0200

    adding stopping & restarting of django fcgi processes

--- fabfile.py
+++ fabfile.py
@@ -1,27 +1,30 @@
 from datetime import datetime
-from fabric.api import env
-from helpers import (_helpers_setup, _helpers_checkout, _helpers_symlink,
-                        _rollback, _helpers_cleanup, _helpers_releases)
+from fabric.api import *
+import helpers
 
 def test():
     env.hosts = ["testing.server"]
 
 
 def development():
-    env.hosts = ["development.server"]
-
+    env.hosts = ["ovm1.praekelt.com"]
+    env.branch = "development"
+    env.fcgi_host = '127.0.0.1'
+    env.fcgi_port = '9991'
+    env.fcgi_protocol = 'fcgi'
+    env.fcgi_pidfile = '/var/run/django/txtalert_dev.pid'
+    env.django_settings = 'environments.development'
 
 def production():
     env.hosts = ["production.server"]
+    env.branch = "production"
 
 
 def _defaults():
     env.project = 'txtalert'
     env.user = "sdehaan"
     env.deploy_to = "/home/sdehaan/development/fabric/txtalert"
-    
     env.repository = "git://github.com/smn/txtalert.git"
-    env.branch = "master"
     
     env.release_name = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     env.releases_path = "%(deploy_to)s/releases" % env
@@ -34,23 +37,37 @@ def _defaults():
 def setup():
     "Create the necessary directory layout for a deploy"
     _defaults()
-    _helpers_setup()
+    helpers.setup()
 
 def deploy():
     try: 
         "Build the project and deploy it to a specified environment"
         _defaults()
-        _helpers_setup()
-        _helpers_checkout()
-        _helpers_symlink()
+        helpers.setup()
+        helpers.checkout()
+        helpers.symlink()
     except Exception, e:
         print e
         _rollback()
 
+def start_django():
+    helpers.staging()
+    _defaults()
+    run('python %(current_path)s/manage.py '
+            'runfcgi --settings=%(django_settings)s host=%(fcgi_host)s '
+            'port=%(fcgi_port)s protocol=%(fcgi_protocol)s '
+            'pidfile=%(fcgi_pidfile)s' % env)
+
+
+def stop_django():
+    _defaults()
+    helpers.staging()
+    run('kill -HUP `cat %(fcgi_pidfile)s`' % env)
+
 def cleanup():
     _defaults()
-    _helpers_cleanup()
+    helpers.cleanup()
 
 def releases():
     _defaults()
-    _helpers_releases()
+    helpers.releases()
--- helpers.py
+++ helpers.py
@@ -1,22 +1,22 @@
 from fabric.api import *
 
-def _staging():
+def staging():
     require("hosts", provided_by = ['test', 'development', 'production'])
 
 rollbacks = []
-def _add_rollback(rollback_function):
+def add_rollback(rollback_function):
     rollbacks.append(rollback_function)
 
 
-def _rollback():
+def rollback():
     "Rollback the executed operations"
     print "rolling back %s operations" % len(rollbacks)
     [rollback_function() for rollback_function in rollbacks]
 
 
-def _helpers_setup():
+def setup():
     "Setup the directory layout for the application to be deployed"
-    _staging()
+    staging()
     cmd = "mkdir -p %s" % " ".join([
         "%(releases_path)s" % env,
         "%(shared_path)s" % env,
@@ -25,32 +25,33 @@ def _helpers_setup():
     ])
     run(cmd % env)
 
-def _helpers_checkout():
+def checkout():
     "Checkout the repository"
-    _add_rollback(lambda: run("rm -rf %(current_release)s" % env))
-    _staging()
+    add_rollback(lambda: run("rm -rf %(current_release)s" % env))
+    staging()
     run("git clone %(repository)s %(current_release)s" % env)
     with cd("%(current_release)s" % env):
-        run("git checkout %(branch)s" % env)
-        run("echo %(branch)s > BRANCH" % env)
+        # have the deploy branch track the branch specified in the config
+        run("git checkout -b deploy origin/%(branch)s" % env)
+        run("echo %(branch)s >> BRANCH" % env)
 
 
-def _helpers_symlink():
+def symlink():
     "Symlink `current` to the latest release"
-    _add_rollback(lambda: run("rm -f %(current_path)s && ln -s $PREVIOUS_RELEASE %(current_path)s" % env))
-    _staging()
+    add_rollback(lambda: run("rm -f %(current_path)s && ln -s $PREVIOUS_RELEASE %(current_path)s" % env))
+    staging()
     run("export PREVIOUS_RELEASE=$(ls -1 %(releases_path)s | tail -1)" % env)
     run("rm -f %(current_path)s && ln -s %(current_release)s %(current_path)s" % env)
 
 
-def _helpers_cleanup():
+def cleanup():
     "Cleanup old releases, keeping the last remaining 5"
-    _staging()
+    staging()
     run("cd %(releases_path)s && ls -1 . | head --line=-5 | xargs rm -rf " % env)
 
 
-def _helpers_releases():
+def releases():
     "Get a list of all the deployed releases"
-    _staging()
+    staging()
     run("ls -x %(releases_path)s" % env)
 
